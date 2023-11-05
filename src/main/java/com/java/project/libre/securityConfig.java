5de5249054b9ffbf237a9e56f109e27c2e2303af
loubna moubarik <moubarikloubna1@gmail.com>
2023-11-05 01:37:14 +0100

    add docker compose file for contenerise mu postgree database sql

---
+++ src/main/java/com/java/project/libre/securityConfig.java
@@ -0,0 +1,17 @@
+package com.java.project.libre;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(
+        // securedEnabled = true,
+        // jsr250Enabled = true,
+        prePostEnabled = true)
+public class securityConfig {
+
+}
